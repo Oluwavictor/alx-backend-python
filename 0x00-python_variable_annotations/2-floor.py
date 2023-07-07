31bd0e6346acc8f4707bf0e94521ed5f87dcd41e
Oluwavictor <46841607+Oluwavictor@users.noreply.github.com>
2023-07-07 04:15:40 +0100

    new_floor commit

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -3,10 +3,9 @@
     Task 2: Write a type-annotated function floor
 '''
 
-import math
 
 def floor(a: float) -> int:
     '''
         Computes the floor of a floating-point number.
     '''
-    return math.floor(a)
+    return floor(a)
